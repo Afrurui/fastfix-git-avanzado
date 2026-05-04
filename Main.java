4038685e81a4648c1fa3e700f130abac2c2f862c
Alvaro Frutos Ruiz <afrurui079@g.educaand.es>
2026-05-04 18:58:12 +0200

    Añade registro de vehículos

--- Main.java
+++ Main.java
@@ -3,10 +3,11 @@ public class Main {
         System.out.println("Sistema de Gestión de Taller FastFix v1.0");
         System.out.println("-----------------------------------------");
  
-        // TODO: Alumno A debe inicializar aquí el gestor de vehículos
+        GestorVehiculos vehiculos = new GestorVehiculos();
+        vehiculos.registrarVehiculo();
  
         // ZONA DE INTEGRACION DE MODULOS
-       // TODO: Alumno C debe inicializar aquí el inventario
+        // TODO: Alumno C debe inicializar aquí el inventario
         // TODO: Alumno B debe registrar una reparación de prueba
  
         System.out.println("Sistema detenido.");
